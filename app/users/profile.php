2110294fab2169f0a344b51bc798f513500885a7
AmirA24-art <abduljelila7@gmail.com>
2026-04-21 15:16:17 +0300

    update profile.php

--- app/users/profile.php
+++ app/users/profile.php
@@ -9,7 +9,7 @@ $data = json_decode(file_get_contents("php://input"), true);
 $email = trim($data['email'] ?? '');
 $password = $data['password'] ?? '';
 
-if (!$email  !$password) {
+if (!$email || !$password) {
     echo json_encode(["error" => "Email and password required"]);
     exit;
 }
@@ -20,21 +20,17 @@ $stmt->execute([$email]);
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (!$user  !password_verify($password, $user['password'])) {
+if (!$user || !password_verify($password, $user['password'])) {
     echo json_encode(["error" => "Invalid credentials"]);
     exit;
 }
 
-/* Create JWT */
-$secret = "my_secret_key"; // change this in real project
-
 $payload = [
     "user_id" => $user['id'],
     "email" => $user['email'],
-    "exp" => time() + (60 * 60) // 1 hour
 ];
 
-$token = generateJWT($payload, $secret);
+$token = generateJWT($payload);
 
 echo json_encode([
     "message" => "Login successful",
